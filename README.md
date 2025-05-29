8a9c0e428bd71aa252ce86e4fbad74a3110bfae6
charles-254 <kamaucharles6884@gmailcom>
2025-05-29 06:02:45 +0300

    Build responsive cat image and fact web app.
    - Integrated API to fetch random cat images and facts
    - Implemented async data fetching with Axios
    - Created dynamic DOM updates with JavaScript
    - Added loading spinner with CSS animation
    - Included input validation for image and facts count
    - Styled layout for responsiveness.

---
+++ README.md
@@ -0,0 +1,42 @@
+# 🐱 Random Cat Image & Fact Web App
+
+This is a simple, responsive web application that displays random cat images using [The Cat API](https://thecatapi.com/) and random cat facts using the [meowfacts](https://meowfacts.herokuapp.com/) API. Built with HTML, CSS, and JavaScript.It features async data fetching via Axios and smooth UX with a custom loader animation.
+
+![cat App UI](./src/images/UI.png)
+
+## 🛠️ Features
+
+- Fetch and display random cat images  
+- Generate fun and random cat facts  
+- Async data fetching with Axios  
+- Fully responsive layout  
+- Loader animation for visual feedback 
+
+
+## Getting Started
+
+To run this project locally:
+
+### Step 1: Clone the repo
+
+```bash
+git clone https://github.com/charles-254/Cat-App.git
+```
+
+### Step 2: Navigate into the project folder
+
+```bash
+cd Cat-App
+```
+
+### Step 3: Install dependencies
+
+```bash
+npm install
+```
+
+### Step 4: Run the development server
+
+```bash
+npm run dev
+```
---
+++ src/main.js
@@ -0,0 +1,79 @@
+import axios from 'axios'
+
+let imageResultsWrapper = document.querySelector(".image-results-wrapper")
+let factsResultWrapper = document.querySelector(".facts-results-wrapper")
+const imgSubmitBtn = document.querySelector("#image-submit-btn")
+const factSubmitBtn = document.querySelector("#fact-submit-button")
+const loaderContainer = document.querySelector(".loader-container")
+
+async function getCatPhotos() {
+    imageResultsWrapper.innerHTML = null
+    factsResultWrapper.innerHTML = null
+
+    try {
+        let numOfPhotos = document.querySelector("#number-of-photos").value
+        
+        if (Number(numOfPhotos) < 1) {
+            numOfPhotos = 1
+        } if (Number(numOfPhotos) > 10) {
+            numOfPhotos = 10
+        }
+        let response = await axios.get(`https://api.thecatapi.com/v1/images/search?limit=${numOfPhotos}`)
+        let data = await response.data
+
+        for (let i = 0; i < numOfPhotos; i++) {
+            let div = document.createElement("div")
+            div.classList.add("image-results")
+            div.style.backgroundImage = `url(${data[i].url})`
+            imageResultsWrapper.appendChild(div)
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+    loaderContainer.style.display = "none"
+}
+
+async function getCatFacts() {
+    imageResultsWrapper.innerHTML = null
+    factsResultWrapper.innerHTML = null
+
+    try {
+        let numOfFacts = document.querySelector("#number-of-facts").value
+        
+        if (Number(numOfFacts) < 1) {
+            numOfFacts = 1
+        }
+        if (Number(numOfFacts) > 50) {
+            numOfFacts = 50
+        }
+
+        let response = await axios.get(`https://meowfacts.herokuapp.com/?count=${numOfFacts}`)
+        let data = await response.data
+        let ol = document.createElement("ol")
+
+        for (let i = 0; i < data.data.length; i++) {
+            let li = document.createElement("li")
+            li.innerHTML = data.data[i]
+            ol.appendChild(li)
+        }
+
+        factsResultWrapper.appendChild(ol)
+
+    } catch (error) {
+        console.log(error)
+    }
+    loaderContainer.style.display = "none"
+}
+
+imgSubmitBtn.addEventListener("click", function () {
+    loaderContainer.style.display = "flex"
+    getCatPhotos()
+    
+})
+
+factSubmitBtn.addEventListener("click", function () {
+    loaderContainer.style.display = "flex"
+    getCatFacts()
+    
+})
